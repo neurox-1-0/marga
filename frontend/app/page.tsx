7fc2005d1b5b7a63e610f7cdace919cc8fdd952d
daniel <daniel.adrian.victor@gmail.com>
2026-07-30 18:29:27 +0530

    fixed frontend bug

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { useState, useEffect, useCallback } from "react";
-import Link from "next/link";
 import {
   connectAgentStream,
   simulateEvent,
